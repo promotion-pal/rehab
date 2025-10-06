6fb074674d2b694efe9abcce0718fbf99f09a52b
promotion-pal <promotion.work.pal@gmail.com>
2025-10-06 17:27:19 +1000

    fix menu phone

--- src/components/site/layout/navbar.tsx
+++ src/components/site/layout/navbar.tsx
@@ -28,27 +28,6 @@ import {
 import { subtitle, title } from "../../primitives";
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
-
   return (
     <HeroUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
@@ -108,29 +87,15 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal aria-label="Github" href={siteConfig.links.github}>
-          <GithubIcon className="text-default-500" />
-        </Link>
         <ThemeSwitch />
         <NavbarMenuToggle />
       </NavbarContent>
 
       <NavbarMenu>
-        {searchInput}
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href="#"
-                size="lg"
-              >
+              <Link href={item.href} size="lg" className="text-black">
                 {item.label}
               </Link>
             </NavbarMenuItem>
--- src/shared/wrapper.tsx
+++ src/shared/wrapper.tsx
@@ -13,7 +13,7 @@ const CommonWrapper: FC<CommonWrapperProps> = ({ children, styleWrapper }) => {
         `relative flex items-center rounded-xl justify-center 
         bg-gradient-to-br from-blue-50 via-green-50 to-amber-50 
         dark:from-blue-950 dark:via-green-950 dark:to-amber-900 
-        overflow-hidden px-4 sm:px-6 lg:px-6 py-1 sm:py-3 lg:py-4`,
+        overflow-hidden px-3 sm:px-6 lg:px-6 py-3 sm:py-3 lg:py-4`,
         styleWrapper
       )}
     >
